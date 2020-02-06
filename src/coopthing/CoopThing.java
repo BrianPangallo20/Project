b3faf844d9c9076308198ce0a9ff755cf6ed0a10
Brian Pangallo <bjp20@students.ptcollege.edu>
2020-02-06 13:28:06 -0500

    Brian Pangallo was here

---
+++ src/coopthing/CoopThing.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coopthing;
+
+/**
+ *
+ * @author brian
+ */
+public class CoopThing {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+ 
+    public static boolean Brian(){ 
+    return false;
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
